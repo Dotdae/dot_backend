da39566377c5b3273ed6b60f321bc68bb29123b7
Christopher Ortega <chriiswho@gmail.com>
2024-09-24 15:29:36 -0700

    Added database connection

--- src/db.js
+++ src/db.js
@@ -1,1 +1,28 @@
 // Database connection.
+
+import mysql from "mysql2"
+import "dotenv/config"
+import { serverTime } from "./config.js"
+
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+
+})
+
+connection.connect((error) => {
+
+    if(error) console.error(error)
+    else{
+
+        console.log(`[${serverTime()}] Databese ${process.env.DB_NAME} is connected!`);
+
+    }
+
+
+})
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import app from "./app.js";
 import { serverTime } from "./config.js"
+import "./db.js"
 
 console.log(`[${serverTime()}] Server on port ->`, app.get("port"));
