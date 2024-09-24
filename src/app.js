cb405b64c5f294c0b3174a87e8c504e7481cad3a
Christopher Ortega <chriiswho@gmail.com>
2024-09-24 16:48:02 -0700

    Fixed db connection and added employee register

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import morgan from "morgan"
 
 // Settings.
 
@@ -9,9 +10,20 @@ const PORT = process.env.PORT || 4000;
 app.set("port", PORT);
 app.set("json spaces", 4);
 
+// Routes.
+
+import userRoutes from './routes/user.routes.js';
+
+
 // Middlewares.
 
 app.use(express.json());
+app.use(morgan('dev'));
+
+
+// Routes.
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT)
 
--- src/db.js
+++ src/db.js
@@ -1,27 +1,14 @@
 // Database connection.
 
-import mysql from "mysql2"
+import {createPool} from "mysql2/promise"
 import "dotenv/config"
-import { serverTime } from "./config.js"
 
+export const pool = createPool({
 
-const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-
-})
-
-connection.connect((error) => {
-
-    if(error) console.error(error)
-    else{
-
-        console.log(`[${serverTime()}] Databese ${process.env.DB_NAME} is connected!`);
-
-    }
-
 
 })
 
