49ab9dda24e9de01f18d962f9ab585c431998dba
Christopher Ortega <chriiswho@gmail.com>
2024-10-28 14:35:24 -0700

    Remove profile endpoint in auth route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { authLogin, authLogout, authProfile } from "../controllers/auth.controller.js";
+import { authLogin, authLogout} from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post('/login', authLogin);
 router.post('/logout', authLogout);
-router.get('/profile/:id', authProfile) // Auth required
-
 
 export default router;
