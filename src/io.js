51c068c132ea689efbc235b1a76b2d9901f6acf6
Christopher Ortega <chriiswho@gmail.com>
2024-12-13 02:33:04 -0700

    Fixed auth socket connection

--- src/io.js
+++ src/io.js
@@ -21,37 +21,30 @@ export default function configurationChat(server) {
     const users = new Map();
 
     io.on('connection', async (socket) => {
-
         const userId = socket.user.id;
-
-        // Aquí recibimos el nombre del usuario directamente desde el frontend
+    
+        // Recibir el nombre del usuario desde el frontend y guardarlo en el Map
         socket.on('set-user-name', (userName) => {
-            users.set(userId, { socketId: socket.id, userName });
-            console.log(`Usuario conectado: ${userName} (ID: ${userId})`);
-            io.emit('user-list', Array.from(users.values()));
+          users.set(userId, { socketId: socket.id, userName });
+          console.log(`Usuario conectado: ${userName} (ID: ${userId})`);
+    
+          // Emitir la lista de usuarios conectados
+          io.emit('user-list', Array.from(users.values()));
         });
-  
-
-        // Event to handle private messages
-        socket.on('private-message', ({ recipientId, msg }) => {
-            const recipientSocketId = users.get(recipientId); // Aquí obtenemos el socket.id del destinatario.
-            if (recipientSocketId) {
-                console.log(`Enviando mensaje a ${recipientId} con socket ID ${recipientSocketId}`);
-                io.to(recipientSocketId).emit('private-message', {
-                    senderId: socket.user.id, // ID del remitente
-                    msg,                       // Mensaje
-                });
-            } else {
-                console.log(`Usuario con ID ${recipientId} no está conectado`);
-            }
-        });
+    
+        // Manejar la desconexión de usuarios
         socket.on('disconnect', () => {
-            users.delete(userId);
-            console.log(`Usuario ${userId} desconectado`);
-            // Enviar lista actualizada de usuarios conectados al frontend
-            io.emit('userListUpdate', Array.from(users.entries())); // Actualiza la lista de usuarios conectados
+          const user = users.get(userId);  // Usar el userId para obtener el usuario
+          if (user) {
+            console.log(`Usuario ${user.userName} desconectado`);
+            users.delete(userId);  // Eliminar al usuario usando su userId
+    
+            // Emitir la lista de usuarios actualizada después de la desconexión
+            io.emit('user-list', Array.from(users.values()));
+          } else {
+            console.log(`El usuario con ID ${userId} no se encontraba en la lista.`);
+          }
         });
-    });
-
+      });
 
 }
